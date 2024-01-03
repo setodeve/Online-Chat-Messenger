82bca248c80aacfb299feb4fc827487aaa27aa24
setodeve <keinick11@outlook.com>
2024-01-03 19:44:25 +0900

    UDPファイル名修正

--- lib/setting/udp/server_udp.py
+++ lib/setting/udp/server_udp.py
@@ -3,22 +3,23 @@ import json
 import threading
 from ..initsetting import InetSetting
 
-class Server(InetSetting):
+
+class Server_UDP(InetSetting):
     def __init__(self) -> None:
-        super().__init__('0.0.0.0',9001)
+        super().__init__("0.0.0.0", 9001)
         self.clients = {}
         threading.Thread(target=self.receive_messages()).start()
 
-    def receive_messages(self)-> None:
+    def receive_messages(self) -> None:
         self.sock.bind(self.setinfo)
 
         while True:
-            try :
+            try:
                 message, cli_addr = self.sock.recvfrom(self.size)
-                message = json.loads(message.decode('utf-8'))
+                message = json.loads(message.decode("utf-8"))
                 if cli_addr not in self.clients:
                     self.clients[cli_addr] = time.time()
-                    if(message["message"]==(message["username"]+"-"+"reg")):
+                    if message["message"] == (message["username"] + "-" + "reg"):
                         continue
                 self.relay_message(message, cli_addr)
 
@@ -26,7 +27,7 @@ class Server(InetSetting):
                 self.close()
 
     def relay_message(self, message, sender):
-        message = (message["username"]+" : "+message["message"]).encode('utf-8')
+        message = (message["username"] + " : " + message["message"]).encode("utf-8")
         for client in self.clients.keys():
             if client != sender:
                 self.sock.sendto(message, client)
