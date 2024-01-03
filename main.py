4b4e12f0ef93546b30885a98de45f20040614dce
setodeve <keinick11@outlook.com>
2024-01-04 06:22:54 +0900

    Main関数リファクタリング

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
-import socket
-import os
 import sys
-from pathlib import Path
-from lib.setting.udp import client
-from lib.setting.udp import server
-class main:
-  def __init__(self) -> None:
-    print('main')
-  # def setting_server_tcp():
-  #   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  #   server_address = '0.0.0.0'
-  #   server_port = 9001
+from lib.client import Client_UDP
+from lib.client import Client_TCP
+from lib.server import Server_UDP
+from lib.server import Server_TCP
 
-  #   dpath = 'temp'
-  #   if not os.path.exists(dpath):
-  #       os.makedirs(dpath)
-
-  #   sock.bind((server_address, server_port))
-
-  #   sock.listen(1)
-
-  # def setting_client_tcp():
-  #   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  #   server_address = '0.0.0.0'
-  #   server_port = 9001
-
-  #   try:
-  #       sock.connect((server_address, server_port))
-  #   except socket.error as err:
-  #       print(err)
-  #       sys.exit(1)
-
+class Main():
+    def __init__(self) -> None:
+        pass
+    
+    def start() -> None:
+      args = sys.argv
+      if args[1] == "client":
+         tcp_client = Client_TCP()
+         tcp_client.client_tcp_start()
+         Client_UDP(tcp_client.name,tcp_client.room_name,tcp_client.token)
+      else:
+         tcp_server = Server_TCP()
+         tcp_server.server_tcp_start()
+         Server_UDP()
 
 if __name__ == "__main__":
-    args = sys.argv
-    main()
-    if args[1] == "client":
-       client = client.Client()
-    else:
-       server = server.Server()
+   Main().start()
